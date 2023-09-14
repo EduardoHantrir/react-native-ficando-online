27e9456e71b2c86eca07c8b0056c8ae0c57f7087
EduardoHantrir <didaxcraft@gmail.com>
2023-09-14 20:54:40 -0300

    Projeto PAM 2

--- src/rotas.js
+++ src/rotas.js
@@ -5,18 +5,12 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Tab = createNativeStackNavigator();
 
 import Principal from './paginas/Principal';
-import Repositorios from './paginas/Repositorios';
-import CriarRepositorio from './paginas/CriarRepositorio';
-import InfoRepositorio from './paginas/InfoRepositorio';
 
 export default function Rotas() {
     return (
         <NavigationContainer>
             <Tab.Navigator>
                 <Tab.Screen name="Principal" options={{ title: "Perfil" }} component={Principal} />
-                <Tab.Screen name="Repositorios" component={Repositorios} />
-                <Tab.Screen name="CriarRepositorio" options={{ title: "Criar Repositório" }} component={CriarRepositorio} />
-                <Tab.Screen name="InfoRepositorio" options={{ title: "Repositório Info" }} component={InfoRepositorio} />
             </Tab.Navigator>
         </NavigationContainer>
     );
